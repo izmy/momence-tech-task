ecfd33e5215ea9f8459402882fa80cbf1944adb2
Jaroslav Hrách <me@jaroslavhrach.cz>
2024-09-24 12:27:11 +0200

    chore: add global env

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+export const SETTINGS = {
+  environment: import.meta.env.VITE_ENVIRONMENT ?? 'development',
+  backendApiUrl: import.meta.env.VITE_BACKEND_API_URL ?? 'http://localhost:3000',
+};
+
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
 		<h1>Hello, world!</h1>
